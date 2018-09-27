ce552edf60e03d7965f7a8f14a9df9806547825b
Wafi <septya.wafi@gmail.com>
2018-09-27 09:33:13 +0700

    replace index to inspirational quote

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Inspiring;
+$quote = Inspiring::quote();
+
 
 class Controller extends BaseController
 {
